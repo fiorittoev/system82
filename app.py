cf24bfa78be1313eda6da8f625bc689acdf21098
fiorittoev <fiorittoev@gmail.com>
2024-01-28 08:57:18 -0500

    Day 2 commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, redirect, render_template, request, jsonify
 
 from chat import get_response
 
@@ -15,5 +15,21 @@ def predict():
     message = {"answer": response}
     return jsonify(message)
 
+@app.route("/")
+def home():
+    return render_template('./templates/main.html')
+
+@app.route("/About")
+def about():
+    return render_template('./templates/NavBarLinks/About.html')
+
+@app.route("/CreateAccount.html")
+def signUp():
+    return render_template('./templates/NavBarLinks/CreateAccount.html')
+
+@app.route("/FAQ.html")
+def faq():
+    return render_template('./templates/FAQ.html')
+ 
 if __name__ == "__main__":
     app.run(debug=True)
